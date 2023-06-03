86afa6e40020b31b7c14d3f501f2c222566c1b80
Yangyang Li <yangyang.li@northwestern.edu>
2023-06-03 00:05:29 -0500

    first commit

--- README.md
+++ README.md
@@ -3,10 +3,11 @@
 <!-- begin table -->
 |file name | code  | preview  |
 |---|---|---|
-|transformer.tex|[code](https://github.com/cauliyang/learn_tikz/blob/main/transformer.tex)|[preview](https://github.com/cauliyang/learn_tikz/blob/main/transformer.tex.pdf)|
-|hello_world.tex|[code](https://github.com/cauliyang/learn_tikz/blob/main/hello_world.tex)|[preview](https://github.com/cauliyang/learn_tikz/blob/main/hello_world.tex.pdf)|
-|qual_graph.tex|[code](https://github.com/cauliyang/learn_tikz/blob/main/qual_graph.tex)|[preview](https://github.com/cauliyang/learn_tikz/blob/main/qual_graph.tex.pdf)|
-|tree1.tex|[code](https://github.com/cauliyang/learn_tikz/blob/main/tree1.tex)|[preview](https://github.com/cauliyang/learn_tikz/blob/main/tree1.tex.pdf)|
-|colored_diagram.tex|[code](https://github.com/cauliyang/learn_tikz/blob/main/colored_diagram.tex)|[preview](https://github.com/cauliyang/learn_tikz/blob/main/colored_diagram.tex.pdf)|
-<!-- end table -->
+|transformer.tex|[code](https://github.com/cauliyang/learn_tikz/blob/main/transformer.tex)|[preview](https://github.com/cauliyang/learn_tikz/blob/main/transformer.pdf)|
+|hello_world.tex|[code](https://github.com/cauliyang/learn_tikz/blob/main/hello_world.tex)|[preview](https://github.com/cauliyang/learn_tikz/blob/main/hello_world.pdf)|
+|nmain.tex|[code](https://github.com/cauliyang/learn_tikz/blob/main/nmain.tex)|[preview](https://github.com/cauliyang/learn_tikz/blob/main/nmain.pdf)|
+|main.tex|[code](https://github.com/cauliyang/learn_tikz/blob/main/main.tex)|[preview](https://github.com/cauliyang/learn_tikz/blob/main/main.pdf)|
+|qual_graph.tex|[code](https://github.com/cauliyang/learn_tikz/blob/main/qual_graph.tex)|[preview](https://github.com/cauliyang/learn_tikz/blob/main/qual_graph.pdf)|
+|tree1.tex|[code](https://github.com/cauliyang/learn_tikz/blob/main/tree1.tex)|[preview](https://github.com/cauliyang/learn_tikz/blob/main/tree1.pdf)|
+|colored_diagram.tex|[code](https://github.com/cauliyang/learn_tikz/blob/main/colored_diagram.tex)|[preview](https://github.com/cauliyang/learn_tikz/blob/main/colored_diagram.pdf)|
 <!-- end table -->
--- scripts/links.py
+++ scripts/links.py
@@ -1,34 +1,67 @@
-from os import writev
 from pathlib import Path
 
 
-def write_table(texs):
+def get_code_url(path: Path):
+    base_url = "https://github.com/cauliyang/learn_tikz/blob/main"
+    return base_url + "/" + path.as_posix()
+
+
+def create_table(texs):
     table = "|file name | code  | preview  |\n" "|---|---|---|\n"
     base_url = "https://github.com/cauliyang/learn_tikz/blob/main"
     for tex in texs:
-        table += f"|{tex.name}|[code]({base_url}/{tex})|[preview]({base_url}/{tex.item}.pdf)|\n"
+        table += f"|{tex.name}|[code]({base_url}/{tex})|[preview]({base_url}/{tex.stem}.pdf)|\n"
 
     return table
 
 
-def main():
-    texs = []
-    for tex in Path(".").rglob("*tex"):
-        texs.append(tex)
+def figure(path: Path):
 
-    tex_table = write_table(texs)
+    code = get_code_url(path)
 
-    sep1 = "<!-- begin table -->\n"
-    sep2 = "<!-- end table -->\n"
+    temp = R"""
+    \section{{Example {}}}
+
+    \begin{{figure}}[H]
+        \centering
+        \includestandalone[width=\textwidth]{{{}}}
+        \caption{{{} \href{{{}}}{{{}}} }}
+        \label{{fig:{}}}
+    \end{{figure}}
+    """.format
+
+    caption = path.stem.replace("_", " ")
+
+    return temp(caption, path.stem, caption, code, "code", caption)
+
+
+def create_tex(texs):
+
+    res = []
+    for tex in texs:
+        if tex.name not in ["main.tex", "nmain.tex"]:
+            res.append(figure(tex))
+
+    return "".join(res)
+
+
+def update_latex(texs):
+    figures = create_tex(texs)
+
+    sep1 = "% <!-- begin content -->\n"
+    sep2 = "% <!-- end content -->\n"
+
     content = []
     add = True
+    file_name = Path("main.tex")
+    temp_name = Path("nmain.tex")
 
-    with open("README.md") as f:
+    with open(file_name) as f:
         for line in f:
             if line == sep1:
                 add = False
                 content.append(line)
-                content.append(tex_table)
+                content.append(figures)
 
             if line == sep2:
                 add = True
@@ -36,8 +69,62 @@ def main():
             if add:
                 content.append(line)
 
-    with open("README.md", "w") as f:
+    with open(temp_name, "w") as f:
         f.writelines(content)
+
+    try:
+        with open(temp_name, "w") as f:
+            f.writelines(content)
+    except Exception as e:
+        print(f"update tex error happend {e}")
+        temp_name.unlink()
+    else:
+        file_name.unlink()
+        temp_name.rename(file_name)
+
+
+def update_readme(texs):
+    table = create_table(texs)
+
+    sep1 = "<!-- begin table -->\n"
+    sep2 = "<!-- end table -->\n"
+
+    content = []
+    add = True
+
+    file_name = Path("README.md")
+    temp_name = Path("NREADME.md")
+
+    with open(file_name) as f:
+        for line in f:
+            if line == sep1:
+                add = False
+                content.append(line)
+                content.append(table)
+
+            if line == sep2:
+                add = True
+
+            if add:
+                content.append(line)
+
+    try:
+        with open(temp_name, "w") as f:
+            f.writelines(content)
+    except Exception as e:
+        print(f"update readme error happend {e}")
+        temp_name.unlink()
+    else:
+        file_name.unlink()
+        temp_name.rename(file_name)
+
+
+def main():
+    texs = []
+    for tex in Path(".").rglob("*tex"):
+        texs.append(tex)
+    update_readme(texs)
+    update_latex(texs)
 
 
 if __name__ == "__main__":
